0a795ba855f8c9b648c255ba516d33bd1de300d6
fiqihh78 <fiqihnk78@gmail.com>
2026-06-03 11:31:46 +0700

    Update Laporan.php

--- app/Models/Laporan.php
+++ app/Models/Laporan.php
@@ -17,6 +17,7 @@ class Laporan extends Model
         'pelapor',
         'foto_pelapor',
         'kategori_id',
+        'petugas_id',
         'judul',
         'deskripsi',
         'lokasi',
@@ -25,18 +26,28 @@ class Laporan extends Model
         'status',
         'prioritas',
         'terverifikasi',
+        'catatan_admin',
     ];
 
+    // Relasi ke user pelapor
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    // Relasi ke kategori
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'kategori_id');
     }
 
+    // Relasi ke admin/petugas
+    public function petugas()
+    {
+        return $this->belongsTo(User::class, 'petugas_id');
+    }
+
+    // Relasi penugasan
     public function penugasan()
     {
         return $this->hasOne(Penugasan::class, 'laporan_id');
