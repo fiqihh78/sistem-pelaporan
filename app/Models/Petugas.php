1d1455d5da7d29d89406edd78c7d85b5973359b0
fiqihh78 <fiqihnk78@gmail.com>
2026-06-06 14:57:50 +0700

    Refactor Petugas model structure and properties

--- app/Models/Petugas.php
+++ app/Models/Petugas.php
@@ -2,29 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Petugas extends Model
 {
+    use HasFactory;
+
+    protected $table = 'petugas';
+
     protected $fillable = [
-    'user_id',
-    'id_petugas',
-    'nama',
-    'email',
-    'foto',
-    'spesialisasi',
-    'status',
-    'beban_kerja',
-    'lokasi_saat_ini',
+        'user_id',
+        'id_petugas',
+        'nama',
+        'email',
+        'foto',
+        'spesialisasi',
+        'status',
+        'beban_kerja',
+        'lokasi_saat_ini',
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function laporans()
-    {
-        return $this->hasMany(Laporan::class);
-    }
 }
